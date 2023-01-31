04e2b28b60b0bf86df7784f704a804f3e9212e2f
seoyamin <seoyamin@ewhain.net>
2023-01-31 09:22:44 +0900

    [spring-security] SecurityConfig 설정

--- spring-security/security1/src/main/java/com/cos/security1/controller/IndexController.java
+++ spring-security/security1/src/main/java/com/cos/security1/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.cos.security1.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller  // View를 리턴하겠다
 public class IndexController {
@@ -15,4 +16,33 @@ public class IndexController {
         return "index";  // src/main/resources/templates/index.mustache 찾아감
     }
 
+    @GetMapping("/user")
+    public @ResponseBody String user() {
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public @ResponseBody String admin() {
+        return "admin";
+    }
+
+    @GetMapping("/manager")
+    public @ResponseBody String manager() {
+        return "manager";
+    }
+
+    @GetMapping("/login")
+    public @ResponseBody String login() {
+        return "login";
+    }
+
+    @GetMapping("/join")
+    public @ResponseBody String join() {
+        return "join";
+    }
+
+    @GetMapping("/joinProc")
+    public @ResponseBody String joinProc() {
+        return "회원가입 완료!";
+    }
 }
